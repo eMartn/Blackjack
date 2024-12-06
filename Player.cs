3e972115cf32d4e70486890751f57f3fcd928948
milkems <emartn97@gmail.com>
2024-12-06 00:37:55 -0500

    added todos and some comments with considerations regarding possible solutions in the Card and Deck classes.

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Player
+{
+	private string userName;
+	private List<Card> hand;
+	private bool isDealer;
+
+
+	private string UserName { get => userName; set => userName=value; }
+	private bool IsDealer { get => isDealer; set => isDealer=value; }
+	private List<Card> Hand { get => hand; set => hand=value; }
+
+	public Player()
+	{
+
+	}
+}
